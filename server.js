cacf3ff07b1624da8e002633618ca1aedc899502
Abdullah5326 <abdullahweb53264189@gmail.com>
2026-03-23 08:35:54 +0500

    Added the cloud database

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 require("dotenv").config({ path: "./config.env" });
 const app = require("./app");
 const mongoose = require("mongoose");
-
-mongoose.connect(process.env.LOCAL_DB_URL).then(() => {
-  console.log("Local database is successfully connected.");
-});
+if (process.env.DB_TYPE === "local")
+  mongoose.connect(process.env.LOCAL_DB_URL).then(() => {
+    console.log("Local database is successfully connected.");
+  });
+else
+  mongoose.connect(process.env.CLOUD_DB_URL).then(() => {
+    console.log("The cloud database is connected successfully");
+  });
 
 app.listen(process.env.PORT || 3000, "localhost", (req, res, next) => {
   console.log("The server is listening on port 3000");
